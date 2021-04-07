faf78acaf38df3a15af1b3c6ca907da1d89a4fe1
Song Inbong <sinb57@naver.com>
2021-04-07 18:17:59 +0900

    프로젝트 생성

---
+++ src/test/java/com/project/dps/DpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

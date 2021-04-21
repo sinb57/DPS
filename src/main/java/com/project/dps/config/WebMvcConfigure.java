771ed422cf030b1674c9fe9e90b318bd3cee9237
Song Inbong <sinb57@naver.com>
2021-04-22 02:24:47 +0900

    feat: WebMvcConfigure - resources 경로 설정

---
+++ src/main/java/com/project/dps/config/WebMvcConfigure.java
@@ -0,0 +1,16 @@
+package com.project.dps.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfigure implements WebMvcConfigurer {
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/public/**")
+                .addResourceLocations("classpath:/static/public/", "classpath:/META-INF/resources/public/");
+        registry.addResourceHandler("/custom/**")
+                .addResourceLocations("classpath:/static/custom/", "classpath:/META-INF/resources/custom/");
+    }
+}

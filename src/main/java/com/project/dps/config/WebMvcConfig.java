4f842cdc528f85e266214c27645ad88e5adf6900
Song Inbong <sinb57@naver.com>
2021-04-27 15:16:44 +0900

    style: class명 변경 (WebMvcConfigure -> WebMvcConfig)

--- src/main/java/com/project/dps/config/WebMvcConfig.java
+++ src/main/java/com/project/dps/config/WebMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfigure implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/public/**")

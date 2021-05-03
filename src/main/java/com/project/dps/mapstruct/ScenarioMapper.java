7f096ef488e3d6cd1571c9e753eb9a1b3d4217b7
Song Inbong <sinb57@naver.com>
2021-05-04 02:57:13 +0900

    feat: Mapper Method for scenarioDto (without stageList)

--- src/main/java/com/project/dps/mapstruct/ScenarioMapper.java
+++ src/main/java/com/project/dps/mapstruct/ScenarioMapper.java
@@ -3,10 +3,13 @@ package com.project.dps.mapstruct;
 import com.project.dps.controller.dto.ScenarioDto;
 import com.project.dps.domain.Scenario;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
-
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ScenarioMapper extends GenericMapper<ScenarioDto, Scenario> {
+
+    @Mapping(target = "stageList", ignore = true)
+    ScenarioDto toSimpleDto(Scenario s);
 
 }
